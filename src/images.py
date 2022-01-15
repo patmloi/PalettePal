7a3149275b443b3b554f4436c9c2a8e176cdaea0
patmloi <patriciamloihy@gmail.com>
2022-01-15 18:40:57 +0800

    Update images.py
    Remove arguments.

--- src/images.py
+++ src/images.py
@@ -23,7 +23,7 @@ def startDriver() -> webdriver.Chrome:
     chromeOptions.add_argument("--headless")
 
     chromeOptions.binary_location = GOOGLE_CHROME_PATH
-    driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=chromeOptions)
+    driver = webdriver.Chrome()
     print("Driver")
     return driver
 
